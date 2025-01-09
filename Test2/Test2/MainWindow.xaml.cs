273f913647ed72265deea442fd5823fc18747a13
HishikoNishi <tranggiabao2007@gmail.com>
2025-01-09 21:43:19 +0700

    Thêm nút

---
+++ Test2/Test2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Xử lý sự kiện TextChanged cho EmailTextBox
+        private void EmailTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            EmailPlaceholder.Visibility = string.IsNullOrWhiteSpace(EmailTextBox.Text) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        // Xử lý sự kiện PasswordChanged cho PasswordBox
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordPlaceholder.Visibility = string.IsNullOrWhiteSpace(PasswordBox.Password) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý logic khi nhấn nút "LOGIN"
+        }
+
+        private void ForgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+            // Xử lý logic khi nhấn nút "Forgot Password?"
+        }
+    }
+}
